09f8730424d10007d68f577594a4ad65a2491e48
Solovev Anton <solovev.anton93@gmail.com>
2025-02-05 13:03:14 +0300

    added 3rd stream api task

--- StreamApi/src/main/java/com/solovev/task1/CollectionToStream.java
+++ StreamApi/src/main/java/com/solovev/task1/CollectionToStream.java
@@ -2,7 +2,6 @@ package com.solovev.task1;
 
 import java.util.*;
 import java.util.function.Function;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static java.util.stream.Collectors.*;
