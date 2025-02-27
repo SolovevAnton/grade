95915bf4547f87f2da9aa9e392785cc1238786c8
Solovev Anton <solovev.anton93@gmail.com>
2025-02-27 19:54:39 +0300

    added initial spring rest|soap app

---
+++ Spring-rest-soap/src/test/java/com/solovev/springrestsoap/SpringRestSoapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solovev.springrestsoap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestSoapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
