d5efc6e9bc61c4ed1410a251fa60413df1411f0a
Solovev Anton <solovev.g.solovev@reksoft.com>
2025-02-27 19:54:39 +0300

    added initial spring rest|soap app

---
+++ Spring-rest-soap/src/main/java/com/solovev/springrestsoap/SpringRestSoapApplication.java
@@ -0,0 +1,14 @@
+package com.solovev.springrestsoap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestSoapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRestSoapApplication.class, args);
+    }
+
+}
+
